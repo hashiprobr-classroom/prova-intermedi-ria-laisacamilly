8f85a0f5b8fa7cc3973c67cf5e1e7dfe07697e45
Láisa Camilly <laisaco1@al.insper.edu.br>
2025-09-29 10:53:00 -0300

    refatoração

--- tocha/src/main/java/br/edu/insper/desagil/pi/tocha/Filtro.java
+++ tocha/src/main/java/br/edu/insper/desagil/pi/tocha/Filtro.java
@@ -1,6 +1,6 @@
 package br.edu.insper.desagil.pi.tocha;
 
-public class Filtro {
+public class Filtro extends Tipo{
     private String tipo;
 
     public Filtro(String tipo) {
@@ -8,142 +8,17 @@ public class Filtro {
     }
 
     public Imagem processa(Imagem imagem) {
+
         if (tipo.equals("media")) {
-            // inicializa saída
+            sobel_x(imagem);
+        }
 
-            int[][] entrada = imagem.getPixels();
+        // Sobel_y
+        else if (tipo.equals("sobel_y")) {
+            sobel_y(imagem);
 
-            int altura = entrada.length;
-            if (altura < 3) {
-                throw new IllegalStateException("altura pequena");
-            }
-
-            int largura = entrada[0].length;
-            if (largura < 3) {
-                throw new IllegalStateException("largura pequena");
-            }
-
-            int[][] saida = new int[altura - 2][largura - 2];
-
-            // inicializa kernel
-
-            double[][] kernel = new double[3][3];
-            kernel[0][0] = 0.111111;
-            kernel[0][1] = 0.111111;
-            kernel[0][2] = 0.111111;
-            kernel[1][0] = 0.111111;
-            kernel[1][1] = 0.111111;
-            kernel[1][2] = 0.111111;
-            kernel[2][0] = 0.111111;
-            kernel[2][1] = 0.111111;
-            kernel[2][2] = 0.111111;
-
-            // calcula saída
-
-            for (int y = 1; y < altura - 1; y++) {
-                for (int x = 1; x < largura - 1; x++) {
-                    double s = 0;
-                    for (int dy = -1; dy < 2; dy++) {
-                        for (int dx = -1; dx < 2; dx++) {
-                            s += entrada[y + dy][x + dx] * kernel[dy + 1][dx + 1];
-                        }
-                    }
-                    saida[y - 1][x - 1] = (int) s;
-                }
-            }
-
-            return new Imagem(saida);
-        } else if (tipo.equals("sobel_x")) {
-            // inicializa saída
-
-            int[][] entrada = imagem.getPixels();
-
-            int altura = entrada.length;
-            if (altura < 3) {
-                throw new IllegalStateException("altura pequena");
-            }
-
-            int largura = entrada[0].length;
-            if (largura < 3) {
-                throw new IllegalStateException("largura pequena");
-            }
-
-            int[][] saida = new int[altura - 2][largura - 2];
-
-            // inicializa kernel
-
-            double[][] kernel = new double[3][3];
-            kernel[0][0] = -1;
-            kernel[0][1] = 0;
-            kernel[0][2] = 1;
-            kernel[1][0] = -2;
-            kernel[1][1] = 0;
-            kernel[1][2] = 2;
-            kernel[2][0] = -1;
-            kernel[2][1] = 0;
-            kernel[2][2] = 1;
-
-            // calcula saída
-
-            for (int y = 1; y < altura - 1; y++) {
-                for (int x = 1; x < largura - 1; x++) {
-                    double s = 0;
-                    for (int dy = -1; dy < 2; dy++) {
-                        for (int dx = -1; dx < 2; dx++) {
-                            s += entrada[y + dy][x + dx] * kernel[dy + 1][dx + 1];
-                        }
-                    }
-                    saida[y - 1][x - 1] = (int) s;
-                }
-            }
-
-            return new Imagem(saida);
-        } else if (tipo.equals("sobel_y")) {
-            // inicializa saída
-
-            int[][] entrada = imagem.getPixels();
-
-            int altura = entrada.length;
-            if (altura < 3) {
-                throw new IllegalStateException("altura pequena");
-            }
-
-            int largura = entrada[0].length;
-            if (largura < 3) {
-                throw new IllegalStateException("largura pequena");
-            }
-
-            int[][] saida = new int[altura - 2][largura - 2];
-
-            // inicializa kernel
-
-            double[][] kernel = new double[3][3];
-            kernel[0][0] = -1;
-            kernel[0][1] = -2;
-            kernel[0][2] = -1;
-            kernel[1][0] = 0;
-            kernel[1][1] = 0;
-            kernel[1][2] = 0;
-            kernel[2][0] = 1;
-            kernel[2][1] = 2;
-            kernel[2][2] = 1;
-
-            // calcula saída
-
-            for (int y = 1; y < altura - 1; y++) {
-                for (int x = 1; x < largura - 1; x++) {
-                    double s = 0;
-                    for (int dy = -1; dy < 2; dy++) {
-                        for (int dx = -1; dx < 2; dx++) {
-                            s += entrada[y + dy][x + dx] * kernel[dy + 1][dx + 1];
-                        }
-                    }
-                    saida[y - 1][x - 1] = (int) s;
-                }
-            }
-
-            return new Imagem(saida);
-        } else if (tipo.equals("laplace")) {
+        // laplace:
+        else if (tipo.equals("laplace")) {
             // inicializa saída
 
             int[][] entrada = imagem.getPixels();
@@ -188,8 +63,64 @@ public class Filtro {
             }
 
             return new Imagem(saida);
-        } else {
+        }
+        else {
             throw new IllegalStateException("tipo inválido");
         }
+    }
+
+    private static Imagem getImagem2(Imagem imagem) {
+        return getImagem1(imagem);
+    }
+
+    private static Imagem getImagem1(Imagem imagem) {
+        return getImagem(imagem);
+    }
+
+    private static Imagem getImagem(Imagem imagem) {
+        // inicializa saída
+
+        int[][] entrada = imagem.getPixels();
+
+        int altura = entrada.length;
+        if (altura < 3) {
+            throw new IllegalStateException("altura pequena");
+        }
+
+        int largura = entrada[0].length;
+        if (largura < 3) {
+            throw new IllegalStateException("largura pequena");
+        }
+
+        int[][] saida = new int[altura - 2][largura - 2];
+
+        // inicializa kernel
+
+        double[][] kernel = new double[3][3];
+        kernel[0][0] = 0.111111;
+        kernel[0][1] = 0.111111;
+        kernel[0][2] = 0.111111;
+        kernel[1][0] = 0.111111;
+        kernel[1][1] = 0.111111;
+        kernel[1][2] = 0.111111;
+        kernel[2][0] = 0.111111;
+        kernel[2][1] = 0.111111;
+        kernel[2][2] = 0.111111;
+
+        // calcula saída
+
+        for (int y = 1; y < altura - 1; y++) {
+            for (int x = 1; x < largura - 1; x++) {
+                double s = 0;
+                for (int dy = -1; dy < 2; dy++) {
+                    for (int dx = -1; dx < 2; dx++) {
+                        s += entrada[y + dy][x + dx] * kernel[dy + 1][dx + 1];
+                    }
+                }
+                saida[y - 1][x - 1] = (int) s;
+            }
+        }
+
+        return new Imagem(saida);
     }
 }
